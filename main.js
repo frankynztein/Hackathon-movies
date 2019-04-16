630ae91835268ee20bfd5413a167498ae3283755
kathmontalvo <kathmontalvo123@gmail.com>
2019-04-16 01:49:38 -0500

    imprime data al hacer click por pelicula

--- main.js
+++ main.js
@@ -5,42 +5,84 @@ const getJson = (datajson) => {
   fetch(datajson)
     .then((res) => res.json())
     .then(data => {
-      infoMovies = data;
-      return infoMovies;
+      return infoMovies = data;
     })
 };
 
-searchMovie.addEventListener('click', () => {
-  const movieValue = document.getElementById('movie-name').value;
-  getJson('http://www.omdbapi.com/?s=' + encodeURI(movieValue) + '&apikey=c99c4c69')
-  const printCards = () => {
-    abc = Object.entries(infoMovies);
-    arrMovies = abc[0][1]
-    console.log(arrMovies)
+const moviesCards = document.getElementById('movies-cards')
+const carouselImgs = document.getElementById('carousel-imgs')
 
-    arrMovies.forEach((movie) => {
-      console.log(movie)
-      let string =
-        ` <img class="card-img-top" src=${movie.Poster} alt="Card image cap">
-          <div class="card-body">
-            <h5 class="card-title">${movie.Title}</h5>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">Year: ${movie.Year}</small>
-          </div>`
-      const div = document.createElement('div');
-      div.innerHTML = string;
-      div.className = 'card';
-      document.getElementById('output').appendChild(div);
-      printMainInfo(div);
-    })
-  }
-  setTimeout(printCards, 500)
-})
-const printMainInfo = (element) => {
+const getSearchValue = () => {
+  const movieValue = document.getElementById('movie-name').value;
+  getJson('http://www.omdbapi.com/?s=' + encodeURI(movieValue) + '&apikey=c99c4c69');
+  setTimeout(printCards, 500);
+  moviesCards.classList.remove('hide')
+  carouselImgs.classList.add('hide')
 
 }
 
+const printCards = () => {
+  arrData = Object.entries(infoMovies);
+
+  arrMovies = arrData[0][1];
+  console.log(arrMovies)
+
+  document.getElementById('output').innerHTML = '';
+  arrMovies.forEach((movie) => {
+    let string =
+      `<div name="movies" id=${movie.imdbID}>
+      <img class="card-img-top" src=${movie.Poster} alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">${movie.Title}</h5>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Year: ${movie.Year}</small>
+        </div>
+      </div>`
+    const div = document.createElement('div');
+    div.innerHTML = string;
+    div.className = 'card my-2 col-12 col-sm-6 col-md-4';
+    document.getElementById('output').appendChild(div);
+    printMainInfo(div);
+  })
+}
+
+const infoGeneral = document.getElementById('info-general')
+const printMainInfo = (element) => {
+  arrData = Object.entries(infoMovies);
+
+  arrMovies = arrData[0][1];
+  const printName = element.querySelector('[name=\'movies\']');
+  // console.log(arrMovies)
+
+  printName.addEventListener('click', () => {
+    moviesCards.classList.add('hide');
+    carouselImgs.classList.add('hide');
+    infoGeneral.innerHTML = '';
+    const atribId = printName.getAttribute('id');
+    getJson('http://www.omdbapi.com/?i=' + encodeURI(atribId) + '&apikey=c99c4c69')
+    setTimeout(() => {
+      console.log(infoMovies)
+      let dataPerMovie = `
+        <div class="row">
+          <div class="col">
+            <img src=${infoMovies.Poster} alt = "" >
+          </div >
+          <div class="col">
+            <h2>${infoMovies.Title}</h2>
+            <p>${infoMovies.Year}</p>
+            <p>Genre: ${infoMovies.Genre}</p>
+            <p>Actors: ${infoMovies.Actors}</p>
+            <p>${infoMovies.Plot}</p>
+            <p>Rating: ${infoMovies.imdbRating}</p>
+          </div>
+        </div >`
+      infoGeneral.innerHTML = dataPerMovie;
+    }, 500)
+  })
+}
+
+searchMovie.addEventListener('click', getSearchValue)
 
 
 
@@ -53,11 +95,11 @@ fetch('http://www.omdbapi.com/?i=tt3896198&apikey=c99c4c69')
     let output = '<h2 class="mb-4">Posts</h2>';
     data.forEach((movie) => {
         output += `
-        div class="card card-body mb-3">
-            <h3>${movie.title}</h3>
-            <p>${movie.plot}</p>
-        /div>
-        `;
+div class="card card-body mb-3" >
+  <h3>${movie.title}</h3>
+  <p>${movie.plot}</p>
+        / div >
+  `;
     });
     document.getElementById('output').innerHTML = output;
 })
@@ -66,11 +108,11 @@ fetch('http://www.omdbapi.com/?i=tt3896198&apikey=c99c4c69')
     let output = '<h2 class="mb-4">Posts</h2>';
     data.forEach((posts) => {
         output += `
-        div class="card card-body mb-3">
-            <h3>${posts.title}</h3>
-            <p>${posts.body}</p>
-        /div>
-        `;
+div class="card card-body mb-3" >
+  <h3>${posts.title}</h3>
+  <p>${posts.body}</p>
+        / div >
+  `;
     });
     document.getElementById('output').innerHTML = output;
 })
